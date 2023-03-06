bff607756ef409212a02a2214e4fa09c191a7ecc
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2023-03-06 11:07:03 +0200

    project sr=tructure

---
+++ assignmentAssaf/CustomDelegates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentAssaf
+{
+    internal class CustomDelegates
+    {
+        public delegate string[] DirectoryFilesNames(string directoryPath);
+    }
+}
---
+++ assignmentAssaf/IDirectoryWatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static assignmentAssaf.CustomDelegates;
+
+namespace assignmentAssaf
+{
+    internal abstract class IDirectoryWatcher
+    {
+        protected string pathOfWatchedDirectory;
+        public IDirectoryWatcher(string pathOfADirectory, DirectoryFilesNames directoryFilesNames)
+        {
+            this.pathOfWatchedDirectory = pathOfADirectory;
+            directoryFilesNames?.Invoke(pathOfADirectory);
+        }
+
+        public abstract void Start();
+        public abstract void Stop();
+    }
+}
