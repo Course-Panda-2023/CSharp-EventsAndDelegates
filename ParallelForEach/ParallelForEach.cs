e910e737f1017106bd1cd18acff47efbf8be63cf
AmirGolomb <gh.amir.251@gmail.com>
2023-03-13 11:21:11 +0200

    Added ParallelForEach

---
+++ ParallelForEach/ParallelForEach.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+delegate void func(string str);
+class ParallelForEach
+{
+    public static void RunParallelFunctions(Collection<string> strings, func function)
+    {
+        foreach (string str in strings)
+        {
+            string tmp = str;
+            Thread myThread = new Thread(() => function(tmp));
+            myThread.Start();
+        }
+
+    }
+}
---
+++ ParallelForEach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        string[] strings = { "a", "bbb", "ccccccc" };
+        Collection<string> collection = new Collection<string>(strings);
+        ParallelForEach.RunParallelFunctions(collection, ExampleMethod);      
+    }
+    public static void ExampleMethod(string str)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i.ToString() + " " + str);
+            Thread.Sleep(1);
+        }
+    }
+}
