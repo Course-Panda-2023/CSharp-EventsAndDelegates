4900d9fa489d9d57cb75eeb2ec4bfe5892a4682a
Tamir <tamirk07gmail.com>
2023-04-14 19:50:26 +0300

    left to do : filters  on dir watcher

---
+++ Delegate/DirectoryWatcher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+class IDirectoryWatcherFactory
+{
+    public delegate string[] GetFiles(string path);
+    static IDirectoryWatcher CreateWatcher(string path, GetFiles func)
+    {
+        return new IDirectoryWatcher(path, func);
+    }
+    static void Main(string[] args)
+    {
+        string path = "C:\\tmp";
+        IDirectoryWatcher watcher = CreateWatcher(path,Directory.GetFiles);
+        watcher.start();
+        Console.ReadLine();
+    }
+}
+class IDirectoryWatcher : FileSystemWatcher
+{
+    //new FileSystemWatcher()
+    IDirectoryWatcherFactory.GetFiles func;
+    public IDirectoryWatcher(string _path, IDirectoryWatcherFactory.GetFiles _func) : base(_path)
+    {
+        func = _func;
+        Changed += HandleEverything;
+        Deleted += HandleEverything;
+        Created += HandleEverything;
+    }
+    public void start()
+    {
+        this.EnableRaisingEvents= true;
+    }
+    public void stop()
+    {
+        this.EnableRaisingEvents= false;
+    }
+    private void HandleEverything(object sender, FileSystemEventArgs e)
+    {
+        WatcherChangeTypes type = e.ChangeType;
+        Console.WriteLine(type.ToString() + "   " + e.FullPath);
+        Console.WriteLine("Current Directory state:\n" + String.Concat( func(Path)));//func(Path).ToList().Select(x=>x.ToString().Concat());
+    }
+}
--- Delegate/Task4/ParallelForEach.cs
+++ Delegate/Task4/ParallelForEach.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-class Program
+class ParallelForEach
 {
     static void Main()
     {
